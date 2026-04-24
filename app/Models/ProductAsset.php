66ae432a9385abe623595ce137e799777ff57374
RafiiMF <rafiimuhammadfadhil611@gmail.com>
2026-04-24 11:01:33 +0700

    p

--- app/Models/ProductAsset.php
+++ app/Models/ProductAsset.php
@@ -4,16 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+###anjay
 class ProductAsset extends Model
 {
     protected $fillable = [
-        'code', 'product_name', 'cost_price', 'price_gr', 'price_ec',
-        'min_stock', 'stock', 'on_order', 'discontinue',
-        'jan_val', 'feb_val', 'mar_val', 'apr_val', 'may_val', 'jun_val',
-        'jul_val', 'aug_val', 'sep_val', 'oct_val', 'nov_val', 'dec_val',
-        'last_received', 'last_issued', 'last_sold', 'ytd_received', 'ytd_issued',
-        'wh_stock', 'warehouse_name'
+        'code',
+        'product_name',
+        'cost_price',
+        'price_gr',
+        'price_ec',
+        'min_stock',
+        'stock',
+        'on_order',
+        'discontinue',
+        'jan_val',
+        'feb_val',
+        'mar_val',
+        'apr_val',
+        'may_val',
+        'jun_val',
+        'jul_val',
+        'aug_val',
+        'sep_val',
+        'oct_val',
+        'nov_val',
+        'dec_val',
+        'last_received',
+        'last_issued',
+        'last_sold',
+        'ytd_received',
+        'ytd_issued',
+        'wh_stock',
+        'warehouse_name'
     ];
     use HasFactory;
 
